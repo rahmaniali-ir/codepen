38c302867aa84a0af6e37a446d908a5bd79c5817
Ali Rahmani <alirahmani.prog@gmail.com>
2023-08-02 20:11:23 +0330

    music files added

--- rap/script.js
+++ rap/script.js
@@ -1,5 +1,6 @@
 const backgrounds = document.querySelectorAll(".back")
 const rappers = document.querySelectorAll(".rapper")
+let attachedMusicEvents = false
 
 backgrounds.forEach((back, i) => {
   setTimeout(() => {
@@ -19,3 +20,49 @@ rappers.forEach(rapper => {
     background.classList.remove("show")
   })
 })
+
+function attachMusicEvents() {
+  if (attachedMusicEvents) return
+  attachedMusicEvents = true
+  console.log("Attached music events!")
+
+  rappers.forEach(rapper => {
+    const name = rapper.dataset.name
+    const music = new Audio(`./music/${name}.mp3`)
+    music.loop = true
+    let fadingIn = false
+    let fadeIn, fadeOut
+
+    rapper.addEventListener("mouseenter", () => {
+      music.volume = 0
+      music.play()
+
+      fadingIn = true
+      fadeIn = setInterval(() => {
+        if (music.volume < 1) music.volume = Math.min(1, music.volume + 0.1)
+        else {
+          clearInterval(fadeIn)
+          fadingIn = false
+        }
+      }, 100)
+    })
+
+    rapper.addEventListener("mouseleave", () => {
+      if (fadingIn) {
+        clearInterval(fadeIn)
+        fadingIn = false
+        music.pause()
+      }
+
+      fadeOut = setInterval(() => {
+        if (music.volume > 0) music.volume = Math.max(0, music.volume - 0.1)
+        else {
+          clearInterval(fadeOut)
+          music.pause()
+        }
+      }, 100)
+    })
+  })
+}
+
+document.body.addEventListener("click", attachMusicEvents)
